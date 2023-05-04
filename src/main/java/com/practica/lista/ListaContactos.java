9b6169e27ff0e3f535ad31d2f7600cba9abdec3f
jmonteroflores <j.montero.flores@alumnos.upm.es>
2023-05-04 12:15:48 +0200

    reutilizo método insertarEnLista en introducirNodoNuevo issue #9

--- src/main/java/com/practica/lista/ListaContactos.java
+++ src/main/java/com/practica/lista/ListaContactos.java
@@ -43,25 +43,7 @@ public class ListaContactos {
 	}
 
 	private void introducirNodoNuevo(NodoTemporal nuevo, PosicionPersona p, NodoTemporal aux, NodoTemporal ant) {
-		NodoPosicion npActual = nuevo.getListaCoordenadas();
-		NodoPosicion npAnt=null;
-		boolean npEncontrado = false;
-		while (npActual!=null && !npEncontrado) {
-			if(npActual.getCoordenada().equals(p.getCoordenada())) {
-				npEncontrado=true;
-				npActual.setNumPersonas(npActual.getNumPersonas()+1);
-			}else {
-				npAnt = npActual;
-				npActual = npActual.getSiguiente();
-			}
-		}
-		if(!npEncontrado) {
-			NodoPosicion npNuevo = new NodoPosicion(p.getCoordenada(),  1, null);
-			if(nuevo.getListaCoordenadas()==null)
-				nuevo.setListaCoordenadas(npNuevo);
-			else
-				npAnt.setSiguiente(npNuevo);
-		}
+		insertarEnLista(p, nuevo);
 		if(ant !=null) {
 			nuevo.setSiguiente(aux);
 			ant.setSiguiente(nuevo);
